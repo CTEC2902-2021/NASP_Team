00bd06fa69b1283f2a838dea24b2f6ac7310ed78
Nazib <nazibrahman39@gmail.com>
2021-04-13 15:47:14 +0100

    Added Colorful Console using

--- SnapSoccer/SnapSoccer/Program.cs
+++ SnapSoccer/SnapSoccer/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Drawing;
+using Console = Colorful.Console;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -13,10 +15,10 @@ namespace SnapSoccer
         {
             Debug.WriteLine("Hello world");
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            //Console.ReadKey();
+            Console.WriteLine("Programme has finished ");
+            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
-        }
+
+        } // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app!
     }
 }
