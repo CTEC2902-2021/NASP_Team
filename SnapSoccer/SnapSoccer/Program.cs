3a6300919af46289ee5e7d38b4e3f6c14bb8be75
Nazib <nazibrahman39@gmail.com>
2021-04-13 15:18:16 +0100

    Added Using System Diag

--- SnapSoccer/SnapSoccer/Program.cs
+++ SnapSoccer/SnapSoccer/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace SnapSoccer
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
+            Debug.WriteLine("Hello world");
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
             //Console.ReadKey();
